4ad2fe4c10ec449bacbed8f7aa05f80e9f994d1a
thomasyu21 <thomasyu085@gmail.com>
2020-10-30 19:04:36 -0400

    Tested extensively. Fixed classify to round and setVertex to change the correct vertex.

--- Tester.java
+++ Tester.java
@@ -4,24 +4,36 @@ public class Tester {
     Point p2 = new Point(6, 0);
     Point p3 = new Point(3, 4);
     Point p4 = new Point(3, 4);
+    Point p5 = new Point(3, 3);
 
     System.out.println(p1.getX());
     System.out.println(p2.getX());
     System.out.println(p3.getX());
 
+    System.out.println();
+    System.out.println(p1.getY());
+    System.out.println(p2.getY());
+    System.out.println(p3.getY());
+
+    System.out.println();
     System.out.println(p1.distanceTo(p2));
     System.out.println(p2.distanceTo(p3));
     System.out.println(p3.distanceTo(p1));
 
+    System.out.println();
     System.out.println(p1.equals(p4));
     System.out.println(p3.equals(p4));
 
 
     Triangle t1 = new Triangle(p1, p2, p3);
+    System.out.println();
     System.out.println(t1.getPerimeter());
     System.out.println(t1.getArea());
     System.out.println(t1.classify());
 
     System.out.println(t1);
+    t1.setVertex(1, p5);
+    System.out.println(p5.getX());
+    System.out.println(p5.getY());
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -26,15 +26,15 @@ public class Triangle {
   }
 
   public String classify() {
-    double side1 = v1.distanceTo(v2);
-    double side2 = v1.distanceTo(v3);
-    double side3 = v2.distanceTo(v3);
+    double side1 = (Math.round(v1.distanceTo(v2) * 10000)) / 10000.;
+    double side2 = (Math.round(v1.distanceTo(v3) * 10000)) / 10000.;
+    double side3 = (Math.round(v2.distanceTo(v3) * 10000)) / 10000.;
     if (side1 == side2 && side2 == side3) {
-      return "Equilateral";
+      return "equilateral";
     }else if (side1 == side2 || side1 == side3 || side2 == side3) {
-      return "Isosceles";
+      return "isosceles";
     }else{
-      return "Scalene";
+      return "scalene";
     }
   }
 
@@ -42,6 +42,16 @@ public class Triangle {
     return "v1("+v1.getX()+", "+v1.getY()
           +") v2("+v2.getX()+", "+v2.getY()
           +") v3("+v3.getX()+", "+v3.getY()+")";
+  }
+
+  public void setVertex(int index, Point newP) {
+    if (index == 0) {
+      v1 = newP;
+    }else if (index == 1) {
+      v2 = newP;
+    }else if (index == 2) {
+      v3 = newP;
+    }
   }
 
 }
