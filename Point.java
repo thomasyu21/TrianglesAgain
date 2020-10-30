98ded55bb7a41392748decb05e6d439c0ff6866e
thomasyu21 <thomasyu085@gmail.com>
2020-10-29 22:18:32 -0400

    Created the Point class and the methods distanceTo and equals, as well as tested them.

---
+++ Point.java
@@ -0,0 +1,34 @@
+public class Point {
+  private double x,y;
+
+  //construct a point given coordinates
+  public Point(double X, double Y) {
+    x = X;
+    y = Y;
+  }
+
+  //construct a point given a point  (redundant , just provides flexibility)
+  public Point(Point p) {
+    x = p.x;  //private access in the same file is allowed!
+    y = p.y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public double distanceTo(Point other) {
+    double diffX = x - other.x;
+    double diffY = y - other.y;
+    return Math.sqrt((diffX*diffX) + (diffY * diffY));
+  }
+
+  public boolean equals(Point other) {
+    return (x == other.x && y == other.y);
+  }
+
+}
---
+++ Tester.java
@@ -0,0 +1,20 @@
+public class Tester {
+  public static void main(String[] args) {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(6, 0);
+    Point p3 = new Point(3, 4);
+    Point p4 = new Point(3, 4);
+
+    System.out.println(p1.getX());
+    System.out.println(p2.getX());
+    System.out.println(p3.getX());
+
+    System.out.println(p1.distanceTo(p2));
+    System.out.println(p2.distanceTo(p3));
+    System.out.println(p3.distanceTo(p1));
+
+    System.out.println(p1.equals(p4));
+    System.out.println(p3.equals(p4));
+
+  }
+}
